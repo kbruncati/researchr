0e4caf1cb40daf4a92f9a421e8039795c1ec1938
kbruncati <kbruncati@smith.edu>
2022-03-29 10:05:47 -0400

    error tests

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -1,7 +1,6 @@
-# test_that("multiplication works", {
-#   x <- 5 #expect that expressions are equal
-#   class(x) <- c("print_emoji", class(x))
-#   expect_output(print(x), "hey")
-#   expect_equal(as.double(x), 5)
-#   expect_s3_class(x,"print_emoji")
-# })
+test_that("multiplication works", {
+  expect_equal(2 * 2, 4)
+})
+
+expect_error(data_return(1929), "Invalid input. Please enter a valid year between 1985 and 2022.")
+expect_error(data_return('aaa'), "Invalid input. Please enter a valid year between 1985 and 2022.")
