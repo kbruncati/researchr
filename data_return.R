f8f5f220432fc3a5fe8065899a6f4e3c4daf0d32
kbruncati <kbruncati@smith.edu>
2022-03-28 20:30:03 -0400

    function work from separate Rmd

--- data_return.R
+++ data_return.R
@@ -3,10 +3,8 @@ library(dplyr)
 #' @export
 
 data_return <- function(x) {
-  #Read the HTML page via rvest to start
-  #How far should I go? Have user input be year of interest for now and filter that way?
   nih <- read_html('https://exporter.nih.gov/ExPORTER_Catalog.aspx?sid=4&index=0')
-  x <- 2022
+  x <- 2021
   x <- toString(x)
   ahref <- nih %>% html_elements('a') %>% html_attr('href') #collect text in a href
   ahref_data <- data.frame(Col1=sapply(ahref, toString), stringsAsFactors = FALSE) #convert character to data frame to filter for desired links to files
@@ -14,12 +12,22 @@ data_return <- function(x) {
   selected_links <- all_links %>% filter(grepl(x, Col1)) #filter for user input, x
   links_list <- as.list(selected_links$Col1) #convert to list of zip files to loop through... gives me list with only one element??
   append_link <- "http://exporter.nih.gov/"
-  vec <- cbind(append_link, links_list[1])
-  link4download <- paste(append_link, links_list[1], sep='')
-  #Make a temporary file (tf) and a temporary folder (tdir)
-  tf <- tempfile(tmpdir = tdir <- tempdir())
-  download.file(link4download, tf)
-  unzip(tf)
-  selected_data <- xmlToDataFrame(paste('RePORTER_PRJ_X_FY', x, '_002.xml')) #new problem area... need to better understand why we need the tempfile/tempdir
+  for (i in 1:length(links_list)) {
+    link4download <- paste(append_link, links_list[i], sep='')
+    #Make a temporary file (tf) and a temporary folder (tdir)
+    tf <- tempfile(tmpdir = tdir <- tempdir())
+    download.file(link4download, tf)
+    unzip(tf)
+  } #need a counter from 01 to 53 for 2021 only
+  if (x == 2021){
+    for (i in 2:9){
+      name <- paste('dataset', toString(i))
+      eval(name) <- xmlToDataFrame(paste('RePORTER_PRJ_X_FY2021_00', toString(i) , '.xml', sep=''))} #how to have name regenerate and get multiple datasets out of this line??
+  }
+  if (x ==2022){
+    name <- xmlToDataFrame(paste('RePORTER_PRJ_X_FY', toString(x), '_002.xml', sep=''))
+  } else {
+    name <- xmlToDataFrame(paste('RePORTER_PRJ_X_FY', toString(x), '.xml', sep=''))
+  }
   #unlink(tf)... do I need this line? Keep for now?
 }
