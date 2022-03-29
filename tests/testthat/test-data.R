948a304464ea3f45de91a009eda0e95dd9cef273
kbruncati <kbruncati@smith.edu>
2022-03-29 11:52:41 -0400

    change to 1999 for quicker test?

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -4,4 +4,4 @@ test_that("multiplication works", {
 
 expect_equal(data_return(1929), "Invalid input. Please enter a valid year between 1985 and 2022.")
 expect_equal(data_return('aaa'), "Invalid input. Please enter a valid year between 1985 and 2022.")
-expect_equal(data_return(2020), xmlToDataFrame('RePORTER_PRJ_X_FY2020_new.xml'))
+expect_equal(data_return(1999), xmlToDataFrame('RePORTER_PRJ_X_FY2020_new.xml'))
