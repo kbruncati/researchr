7c4e9027ae1c82524a7373af6a4aa0bc6194abe1
kbruncati <kbruncati@smith.edu>
2022-03-29 11:04:29 -0400

    test three

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -4,3 +4,4 @@ test_that("multiplication works", {
 
 expect_equal(data_return(1929), "Invalid input. Please enter a valid year between 1985 and 2022.")
 expect_equal(data_return('aaa'), "Invalid input. Please enter a valid year between 1985 and 2022.")
+expect_equal(data_return(2020), name)
