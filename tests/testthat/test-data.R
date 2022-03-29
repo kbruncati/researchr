92c47540b442761e16dbf47d70258a8a848de9cb
kbruncati <kbruncati@smith.edu>
2022-03-29 18:53:39 -0400

    bring back in third test

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -1,3 +1,3 @@
 expect_equal(data_return(1929), "Invalid input. Please enter a valid year between 1985 and 2022.")
 expect_equal(data_return('aaa'), "Invalid input. Please enter a valid year between 1985 and 2022.")
-#expect_equal(data_return(1999), xmlToDataFrame('RePORTER_PRJ_X_FY1999.xml'))
+expect_equal(data_return(1999), xmlToDataFrame('RePORTER_PRJ_X_FY1999.xml'))
