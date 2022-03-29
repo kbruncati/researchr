ee75246886e8f95e05622de1c4f8157f1e27f065
kbruncati <kbruncati@smith.edu>
2022-03-29 13:00:54 -0400

    take out extra line

--- R/data_return.R
+++ R/data_return.R
@@ -15,11 +15,10 @@
 
 data_return <- function(x) {
   nih <- read_html('https://exporter.nih.gov/ExPORTER_Catalog.aspx?sid=4&index=0')
-  x <- toString(x)
   ahref <- nih %>% html_elements('a') %>% html_attr('href') #collect text in a href
   ahref_data <- data.frame(Col1=sapply(ahref, toString), stringsAsFactors = FALSE) #convert character to data frame to filter for desired links to files
   all_links <- ahref_data %>% dplyr::filter(grepl('XMLData/final/', Col1)) #use dplyr to filter for XML .zip files, now collected under links
-  selected_links <- all_links %>% dplyr::filter(grepl(x, Col1)) #filter for user input, x
+  selected_links <- all_links %>% dplyr::filter(grepl(toString(x), Col1)) #filter for user input, x
   links_list <- as.list(selected_links$Col1) #convert to list of zip files to loop through... gives me list with only one element??
   append_link <- "http://exporter.nih.gov/"
   for (i in 1:length(links_list)) {
