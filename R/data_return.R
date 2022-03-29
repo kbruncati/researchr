50f7eb0014bfee1bd300990ca65a1ad6d34eea58
kbruncati <kbruncati@smith.edu>
2022-03-29 11:36:52 -0400

    _new

--- R/data_return.R
+++ R/data_return.R
@@ -34,7 +34,7 @@ data_return <- function(x) {
     name <- xmlToDataFrame(paste('RePORTER_PRJ_X_FY', toString(x), '_002.xml', sep=''))
   }
   if (x >= 1985 & x <= 2020){
-    name <- xmlToDataFrame(paste('RePORTER_PRJ_X_FY', toString(x), '.xml', sep=''))
+    name <- xmlToDataFrame(paste('RePORTER_PRJ_X_FY', toString(x), '_new.xml', sep=''))
   } else {
     print('Invalid input. Please enter a valid year between 1985 and 2022.')
   }
