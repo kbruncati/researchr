4fac4a4f28de5970a3c021d440a303c2bf9dd28a
kbruncati <kbruncati@smith.edu>
2022-03-29 14:52:14 -0400

    delete sample test

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -1,7 +1,3 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
-})
-
 expect_equal(data_return(1929), "Invalid input. Please enter a valid year between 1985 and 2022.")
 expect_equal(data_return('aaa'), "Invalid input. Please enter a valid year between 1985 and 2022.")
 expect_equal(data_return(1999), xmlToDataFrame('RePORTER_PRJ_X_FY1999.xml'))
