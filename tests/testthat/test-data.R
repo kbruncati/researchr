2963aab0e898f8578171a088e1182256d2a23eb4
kbruncati <kbruncati@smith.edu>
2022-03-29 13:13:18 -0400

    missing apostrophe

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -2,6 +2,6 @@ test_that("multiplication works", {
   expect_equal(2 * 2, 4)
 })
 
-expect_error(data_return(1929), "cannot open URL 'http://exporter.nih.gov/NULL")
+expect_error(data_return(1929), "cannot open URL 'http://exporter.nih.gov/NULL'")
 expect_equal(data_return('aaa'), "Invalid input. Please enter a valid year between 1985 and 2022.")
 expect_equal(data_return(1999), xmlToDataFrame('RePORTER_PRJ_X_FY1999.xml'))
