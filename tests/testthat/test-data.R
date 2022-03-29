835fef4f8f3d23dbf4e9fea51f900fb97883c7d5
kbruncati <kbruncati@smith.edu>
2022-03-29 11:00:33 -0400

    fix tests?

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -2,5 +2,5 @@ test_that("multiplication works", {
   expect_equal(2 * 2, 4)
 })
 
-expect_error(data_return(1929), "Invalid input. Please enter a valid year between 1985 and 2022.")
-expect_error(data_return('aaa'), "Invalid input. Please enter a valid year between 1985 and 2022.")
+expect_equal(data_return(1929), "Invalid input. Please enter a valid year between 1985 and 2022.")
+expect_equal(data_return('aaa'), "Invalid input. Please enter a valid year between 1985 and 2022.")
