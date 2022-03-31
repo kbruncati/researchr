c8515c9b00569ff1152751606112ae8c1be64f80
kbruncati <kbruncati@smith.edu>
2022-03-31 11:16:24 -0400

    third test - class

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -1,3 +1,4 @@
 expect_equal(data_return(1929), "Invalid input. Please enter a valid year between 1985 and 2022.")
 expect_equal(data_return('aaa'), "Invalid input. Please enter a valid year between 1985 and 2022.")
+expect_is(data, 'data.frame')
 #expect_equal(nrow(data_2021), )
