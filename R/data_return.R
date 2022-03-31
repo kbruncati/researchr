79c80efa3562f33ba7fdc9e418f449d4da8cccdc
kbruncati <kbruncati@smith.edu>
2022-03-31 10:51:26 -0400

    potential solution to our problems?

--- R/data_return.R
+++ R/data_return.R
@@ -23,7 +23,7 @@ data_return <- function(x) {
       html_attr('href') #collect text in a href
     ahref_data <- data.frame(Col1=sapply(ahref, toString), stringsAsFactors = FALSE) #convert character to data frame to filter for desired links to files
     all_links <- ahref_data %>%
-      filter(grepl('XMLData/final/', Col1)) #use dplyr to filter for XML .zip files, now collected under links
+      filter(grepl('CSVs/final/RePORTER_PRJ_C', Col1)) #use dplyr to filter for XML .zip files, now collected under links
     selected_links <- all_links %>%
       filter(grepl(toString(x), Col1)) #filter for user input, x
     links_list <- as.list(selected_links$Col1)
@@ -33,20 +33,21 @@ data_return <- function(x) {
       #Make a temporary file (tf) and a temporary folder (tdir)
       td <- tempdir()
       # create a temporary file
-      tf <- tempfile(tmpdir=td, fileext=".zip")
-      download.file(link4download, tf)
-      file_names <- unzip(tf, list=TRUE) #list zip archive
-      unzip(tf, exdir=td, overwrite=TRUE) #extract files from zip file
+      tf <- tempfile()
+      download.file(link4download, tf, quiet=TRUE, mode='wb')
+      #file_names <- unzip(tf, list=TRUE) #list zip archive
+      #unz(tf, paste('CSVs/final/RePORTER_PRJ_C_FY', toString(x), '.csv', sep='')) #extract files from zip file
+      data <- data.table::fread(unzip(file.path(tf)))
     }
-    if (x == 2021){ #multiple files in zip case
-      data_2021 <- lapply(file_names$Name, function(x) import(file.path(td, x)))
-      unlink(td) #delete temp files/directories
-      data_2021 <- as.data.frame(data_2021)
-      return(data_2021)
-    } else { #one file in zip case
-      data <- rio::import(file.path(td, file_names$Name[1]))
-      assign(toString(x), data)
-      unlink(td) #delete temp files/directories
+    # if (x == 2021){ #multiple files in zip case
+    #   data_2021 <- lapply(file_names$Name, function(x) import(file.path(td, x)))
+    #   unlink(td) #delete temp files/directories
+    #   data_2021 <- as.data.frame(data_2021)
+    #   return(data_2021)
+    # } else { #one file in zip case
+    #   data <- rio::import(file.path(td, file_names$Name[1]))
+    #   assign(toString(x), data)
+    #   unlink(td) #delete temp files/directories
 
       #assign(paste0("data.", toString(x)), data2)
       # i tried to change the name of the dataframe and to return it
