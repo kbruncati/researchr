8feb28d0a63b9425fa79a9cf4ba65eed7ed2f60b
hongtonglin <cccynthialht@gmail.com>
2022-04-12 00:28:56 -0400

    downloading data is fine, but we are losing some data when reading the csv files. I don't know how to deal with this situation :/ i tired other ways to read csv files. they either took a very very long time or still lost some data

--- R/data_return.R
+++ R/data_return.R
@@ -15,30 +15,35 @@
 
 data_return <- function(x) {
   if (x >= 1985 & x < 2021){
-    nih <- read_html('https://exporter.nih.gov/ExPORTER_Catalog.aspx?sid=4&index=0') #link to scrape with rvest
+    nih <- read_html('https://reporter.nih.gov/exporter') #link to scrape with rvest
     ahref <- nih %>%
       html_elements('a') %>%
       html_attr('href') #collect text in a href - where desired .zip file links are
     ahref_data <- data.frame(Col1=sapply(ahref, toString), stringsAsFactors = FALSE) #convert character to data frame to filter for desired links to files
-    all_links <- ahref_data %>%
-      filter(grepl('CSVs/final/RePORTER_PRJ_C', Col1)) #use dplyr to filter for XML .zip files, now collected under links
-    selected_links <- all_links %>%
-      filter(grepl(toString(x), Col1)) #filter for user input, x (year)
-    links_list <- as.list(selected_links$Col1) #.zip file links in list for string concatenation
-    append_link <- "http://exporter.nih.gov/" #beginning of link to paste() with links_list for download
-    for (i in 1:length(links_list)) {
-      link4download <- paste(append_link, links_list[i], sep='') #string concatenation
+    #all_links <- ahref_data %>%
+    # filter(grepl('CSVs/final/RePORTER_PRJ_C', Col1)) #use dplyr to filter for XML .zip files, now collected under links
+    #selected_links <- all_links %>%
+    # filter(grepl(toString(x), Col1)) #filter for user input, x (year)
+    #links_list <- as.list(selected_links$Col1) #.zip file links in list for string concatenation
+    append_link <- "https://reporter.nih.gov/services/exporter/Download?fileId=" #beginning of link to paste() with links_list for download
+    year <- 1985:2020 # make a list of years
+
+    for (i in 1:length(year)) {
+      fileld <- 2*which(year == x) # the pattern of urls
+      link4download <- paste0(append_link, 2) #string concatenation
       td <- tempdir() #create a temporary directory
       tf <- tempfile() #create a temporary file
       download.file(link4download, tf, quiet=TRUE, mode='wb') #download file with temp file as dest file
-      data2 <- data.table::fread(unzip(file.path(tf), exdir = td)) #unzip temp file, convert .csv
-      data2 <- as.data.frame(data2) #make final product - data frame
+      data2 <- data.table::fread(unzip(file.path(tf), exdir = td), guess = 100000)
+      #unzip temp file, convert .csv
+      #data2 <- as.data.frame(data2) #make final product - data frame
       return(data2) #return requested data to the user for relevant year
     }
   } else { #user inputs an unavailable year or something entirely unrelated
     print('Invalid input. Please enter a valid year between 1985 and 2020.')
   }
 }
+
 
 
 
