c60e0de44047b46f26a37f96b2a4aae72accf73e
kbruncati <kbruncati@smith.edu>
2022-04-14 11:14:39 -0400

    pass expect_error R CMD?

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -1,4 +1,4 @@
-expect_error(nih_research(1929), "Invalid input. Please enter a valid year between 1985 and 2022.")
-expect_error(nih_research('aaa'), "Invalid input. Please enter a valid year between 1985 and 2022.")
+expect_error(nih_research(1929))
+expect_error(nih_research('aaa'))
 expect_s3_class(nih_research(1999), 'data.frame')
 #expect_equal(nrow(data_2021), )
