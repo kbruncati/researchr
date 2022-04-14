c9d0854daea9251a3d36c390b4e404b35fa4f135
kbruncati <kbruncati@smith.edu>
2022-04-14 11:21:52 -0400

    new test idea?

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -1,4 +1,4 @@
 expect_error(nih_research(1929))
 expect_error(nih_research('aaa'))
 expect_s3_class(nih_research(1999), 'data.frame')
-#expect_equal(nrow(data_2021), )
+expect_equal(nrow(nih_research(2021)), 82122)
