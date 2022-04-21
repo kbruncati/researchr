c6869997758345a90af4a1cedcce7daf43813b34
hongtonglin <cccynthialht@gmail.com>
2022-04-21 16:29:34 -0400

    legend text size changed; comments added

--- R/funding_frequency.R
+++ R/funding_frequency.R
@@ -3,7 +3,7 @@
 #' @name funding_frequency
 #' @param data_frame 2020 US State data saved within the package
 #' @usage funding_frequency(data_frame)
-#' @return a graph of funding frequency by state
+#' @return an interactive graph of funding frequency by state
 #'
 #' @import dplyr
 #' @import utils
@@ -14,30 +14,38 @@
 #' @export
 
 funding_frequency <- function(data_frame){
-  wd <- getwd()
-  states <- sf::read_sf(paste0(wd, "/data/cb_2020_us_state_20m.shp"))
+  wd <- getwd() # get the working directory for this R file
+  states <- sf::read_sf(paste0(wd, "/data/cb_2020_us_state_20m.shp")) # get the path name of the map file
 
   data4 <- table(data_frame$ORG_STATE)
-  data4 <- as.data.frame(data4)
-  state.ex <- c("AB","AS","BC", "GU", "MB", "MH", "MP", "NS", "PW", "QC","ON",
-                "QC", "VI", "PQ")
-  states.final <- data4[!grepl(paste(state.ex, collapse='|'), data4$Var1),]
+  # only select the state names from the nih website, and count the frequency of state names
 
+  data4 <- as.data.frame(data4) # make the table as the data frame
+  state.ex <- c("AB","AS","BC", "GU", "MB", "MH", "MP", "NS", "PW", "QC","ON",
+                "QC", "VI", "PQ") # create a list of state names that are not in the census
+  states.final <- data4[!grepl(paste(state.ex, collapse='|'), data4$Var1),] # remove those nonexistent state names
 
   states.map <- states[order(states$STUSPS), ] %>%
     sf::st_zm() %>%
-    mutate(frequency = states.final$Freq)
+    mutate(frequency = states.final$Freq) # create a map filled with funding frequency
 
+  g <- ggplot(states.map, label = states.final) +
+    geom_sf(aes(fill=frequency)) + # plot out the map filled with funding frequency
+    scale_fill_distiller("the number of times \neach state got funded", palette="Spectral") +
+    ggtitle(label = "Funding Frequency by State") +
+    coord_sf(xlim = c(-170, -50), ylim = c(10,70), expand = FALSE) + # change the map size and view
+    theme(plot.title = element_text(face = "bold"),
+          legend.title = element_text(size = 8), # change the font size of the legend title
+          legend.text = element_text(size = 6)) # change the font size of the numbers in the legend
 
-  g <- ggplot(states.map) +
-    geom_sf(aes(fill=frequency)) +
-    scale_fill_distiller("the number of times each state got funded", palette="Spectral") +
-    ggtitle("Funding Frequency by State") +
-    coord_sf(xlim = c(-150, 0), ylim = c(20,70), expand = FALSE)
+  fund.freq.map <- plotly::ggplotly(g) %>%
+    plotly::layout(title = list(text = paste0('Funding Frequency by State',
+                                              '<br>',
+                                              '<sup>',
+                                              'Select a state to view the funding frequency',
+                                              '</sup>'))) # turn the graph into an interactive one
 
-
-  pp <- plotly::ggplotly(g)
-
-  return(pp)
+  return(fund.freq.map)
 
 }
+
